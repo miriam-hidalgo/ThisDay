12c5ca6a34bb883ecec5cd6f592c6f6cda9ded25
acarta <anthonym.cartagena@gmail.com>
2018-10-20 14:08:08 -0400

    functionality added. needs to be refined

---
+++ dudePizza/js/pizza.js
@@ -0,0 +1,62 @@
+var url = "https://api.nasa.gov/planetary/apod?api_key=54lBa2rMDnkIC43dEmrnkzykVy4aWrfLWxYDJfXO";
+var date;
+var picInfo;
+var picTitle;
+
+$('.datepicker').datepicker({format: 'yyyy-mm-dd'});
+
+$("#submit").on("click",function(){
+
+    //get rid of inputs
+    $(".datepicker").toggle();
+    $("#submit").toggle()
+
+    date = $('.datepicker')[0].value;
+    console.log("date picked = "+date);
+
+    // changeBackground(date);
+    $.ajax({
+        url: url+"&date="+date,
+        success: function(result){
+    
+            //change background color
+            console.log(result)
+            $("body").css("background-image","url("+result.url+")");
+            $("body").css("background-size","cover")
+
+            picInfo = result.explanation;
+            picTitle = result.title;
+            console.log("title = "+picTitle)
+            console.log("title = "+picInfo)
+
+            addCard()
+            
+        }
+    });
+        
+})
+
+function changeBackground(datePicked){
+    $.ajax({
+    url: url+"&date="+datePicked,
+    success: function(result){
+
+        //change background color
+        console.log(result.url)
+        $("body").css("background-image","url("+result.url+")");
+        $("body").css("background-size","cover")
+        }
+    });
+}
+
+function addCard(){
+    console.log("making card now")
+    var newCard = $("<div class='card'></div>");
+    newCard.append("<div class='card-title'>"+picTitle+"</div>")
+    newCard.append("<div class='card-content'>"+picInfo+"</div>")
+
+    $(".contentSpot").append(newCard)
+
+    
+}
+
